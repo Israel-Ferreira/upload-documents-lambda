6e5e7a22b89b27e505a669c854fcfdf47b0afa6d
Israel-Ferreira <israelfsouza@hotmail.com>
2025-12-03 21:37:23 -0300

    Alterando o código da função lambda

--- lambda/main.py
+++ lambda/main.py
@@ -1,10 +1,5 @@
 import boto3
-
-import cgi
-
-from email.parser import BytesParser
-from email.policy import default
-
+import json
 
 import base64
 
@@ -14,71 +9,36 @@ import base64
 BUCKET_NAME = "imoveis-financiamentos-docs"
 
 
+s3 = boto3.client("s3")
+
 def handler(event, context):
-    s3_client = boto3.client('s3')
-    body = event['body']
 
-    if event.get('isBase64Encoded', False):
-        body = base64.b64decode(body)
-    else:
-        body = body.encode('utf-8')
+    filename = event.get("filename")
+
+    filecontent_b64 = event.get("file_content")
 
 
-    content_type = event['headers'].get('Content-Type') or event['headers'].get('content-type')
+    decoded_file = base64.b64decode(filecontent_b64)
 
 
-    _, pdict = cgi.parse_header(content_type)
+    try:
 
-    boundary = pdict.get("boundary")
+        s3.put_object(
+            Bucket=BUCKET_NAME,
+            Key=filename,
+            body=decoded_file
+        )
 
-    if not boundary:
-        return {"statusCode": 400, "body": "Boundary não encontrado no multipart"}
+        return {
+            'statusCode': 200,
+            'body': f'Upload realizado com sucesso para o bucket {BUCKET_NAME}.'
+        }
     
-
-    raw = (
-        f"Content-Type: {content_type}\r\n\r\n".encode("utf-8")
-        + body
-    )
-
-
-    msg = BytesParser(policy=default).parsebytes(raw)
-
-    uploaded_files = []
-
-    for part in msg.iter_parts():
-        disposition = part.get("Content-Disposition")
-        if not disposition:
-            continue
-
-
-        params = dict(part.get_params(header="Content-Disposition"))
-        filename = params.get("filename")
-
-
-        if filename:
-            file_bytes = part.get_payload(decode=True)
-            file_type = part.get_content_type()
-
-
-            s3_client.put_object(
-                Bucket=BUCKET_NAME,
-                Key=filename,
-                Body=file_bytes,
-                ContentType=file_type
-            )
-
-
-            uploaded_files.append(filename)
-
-
-    if not uploaded_files:
-        return {"statusCode": 400, "body": "Boundary não encontrado no multipart"}
-
-
-    return {
-        'statusCode': 200,
-        'body': f'Upload realizado com sucesso para o bucket {BUCKET_NAME}.'
-    }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            "body": json.dumps(f'Error uploading file: {str(e)}')
+        }
 
  
 
